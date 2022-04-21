a7a948c39860702ac3d475f451cf030ace8bd74c
spencer.trinh <spencer.trinh@kinnate.com>
2022-04-20 23:00:10 -0700

    added post endpoint to update flag value

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, HTTPException
+from fastapi import FastAPI, Response, Request, HTTPException, status
 from .db import *
 
 app = FastAPI()
@@ -14,3 +14,17 @@ async def get_data(ft_nbr: str, type: str) -> Response:
         raise HTTPException(status_code=404, detail=f"{ft_nbr} is invalid")
     results = get_table_data(ft_nbr, type)
     return results
+
+@app.post("/v1/update-data", tags=["post-data"])
+async def update_data(request: Request):
+    payload = await request.json()
+    if not payload["BATCH_ID"].startswith('FT') and \
+        len(payload["BATCH_ID"]) != 8:
+        raise HTTPException(status_code=404,
+                            detail=f"{payload['BATCH_ID']} is invalid")
+    if payload["FLAG"] not in [0, 1]:
+        raise HTTPException(status_code=405,
+                            detail=f"{payload['FLAG']} is not acceptable")
+    post_result = update_table_data(payload)
+    post_result = post_result | dict(STATUS_CODE=status.HTTP_200_OK)
+    return post_result
