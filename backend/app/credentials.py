b097b6915349db58f0f8393a8a6aed5c3c72b7e6
spencer.trinh <spencer.trinh@kinnate.com>
2022-04-24 18:34:45 -0700

    use of env vars for oracle creds and logic to read file on local test

--- backend/app/credentials.py
+++ backend/app/credentials.py
@@ -1,11 +1,21 @@
-cred_file = '/Users/spencer.trinhkinnate.com/Documents/security_files/oracle2'
+from os import getenv
+cred_file = getenv('ORACLE_CREDS_ARG', '')
+
 
 cred_dct = {}
 
-with open(cred_file, 'r') as f:
-    lines = f.readlines()
-    for line in lines:
-        str_split = line.split(',')
-        key = str_split[0].strip()
-        value = str_split[1].strip()
-        cred_dct[key] = value
+if cred_file == '':
+    cred_file = '/Users/spencer.trinhkinnate.com/Documents/security_files/oracle2'
+    with open(cred_file, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            str_split = line.split(',')
+            key = str_split[0].strip()
+            value = str_split[1].strip()
+            cred_dct[key] = value
+else:
+    cred_dct['HOST'] = getenv('ORACLE_HOST')
+    cred_dct['USERNAME'] = getenv('ORACLE_USER')
+    cred_dct['PASSWORD'] = getenv('ORACLE_PASS')
+    cred_dct['SID'] = getenv('ORACLE_SID')
+    cred_dct['PORT'] = getenv('ORACLE_PORT')
