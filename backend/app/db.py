02c686c74e2d3d3b1c560181163f87a8c1c5a2f0
spencer.trinh <spencer.trinh@kinnate.com>
2022-04-27 17:22:48 -0700

    add if cond print sql stmt and PROP1 key

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,43 +1,7 @@
 from .oracle_class import OracleConnection
 from .credentials import cred_dct
+from os import getenv
 
-
-def update_flag(rowdata_dct):
-
-    assert len(rowdata_dct.keys()) > 5,\
-        f"The current row data seems empty, data length: {len(rowdata_dct)}"
-
-    with OracleConnection(cred_dct['USERNAME'],
-                          cred_dct['PASSWORD'],
-                          cred_dct['HOST'],
-                          cred_dct['PORT'],
-                          cred_dct['SID']) as con:
-
-        sql_stmt = """UPDATE TEST_BIOCHEM_IC50_FLAGS SET FLAG = :1,
-                            WHERE EXPERIMENT_ID = :2
-                            AND BATCH_ID = :3
-                            AND TARGET = :4
-                            AND VARIANT = :5
-                            AND PROP1 = :6"""
-
-        data_lst = [
-            rowdata_dct['FLAG'],
-            rowdata_dct['EXPERIMENT_ID'],
-            rowdata_dct['BATCH_ID'],
-            rowdata_dct['TARGET'],
-            rowdata_dct['VARIANT'],
-            rowdata_dct['PROP1']
-        ]
-
-        for d in data_lst:
-            print(d)
-
-        # assert all([isinstance(d, str) for d in data_lst]), \
-        #     "Can only upload string type values"
-        with con.cursor() as cursor:
-            cursor.execute(sql_stmt, data_lst)
-        con.commit()
-        print(f'successfully uploaded data: {rowdata_dct.items()}')
 
 def get_table_data(cmp_id, type):
 
@@ -55,7 +19,8 @@ def get_table_data(cmp_id, type):
             sql_stmt += "TEST_BIOCHEM_IC50_FLAGS"
 
         sql_stmt += f" WHERE BATCH_ID LIKE '{cmp_id}%' ORDER BY EXPERIMENT_ID, TARGET"
-        # print(sql_stmt)
+        if getenv("INSTANCE_TYPE", None) == None: 
+            print(sql_stmt)
 
         with con.cursor() as cursor:
             cursor.execute(sql_stmt)
@@ -72,6 +37,7 @@ def get_table_data(cmp_id, type):
                 output_dct['TARGET'] = r[2]
                 output_dct['VARIANT'] = r[3]
                 output_dct['FLAG'] = r[4]
+                output_dct['PROP1'] = r[5]
                 output_lst.append(output_dct)
         return output_lst
 
@@ -94,8 +60,10 @@ def update_table_data(payload):
                          AND EXPERIMENT_ID = '{payload["EXPERIMENT_ID"]}'
                          AND TARGET = '{payload["TARGET"]}'
                          AND VARIANT {'IS NULL' if payload["VARIANT"] == 'None' or payload["VARIANT"] is None else f"= '{payload['VARIANT']}'"}
+                         AND PROP1 = {payload["PROP1"]}
                      """
-        print(sql_stmt)
+        if getenv("INSTANCE_TYPE", None) == None: 
+            print(sql_stmt)
 
         with con.cursor() as cursor:
             cursor.execute(sql_stmt)
