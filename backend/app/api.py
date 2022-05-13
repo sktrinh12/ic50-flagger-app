bc99b1a54d6d715e17f0abcaaa82476e5e6507ac
spencer.trinh <spencer.trinh@kinnate.com>
2022-05-13 10:50:20 -0700

    conditional logic to fetch entire table or several rows that need geomean updating, included many more param query args

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Response, Request, HTTPException, status
+from typing import Optional
 from .db import *
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -19,19 +20,53 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/v1/fetch-data", tags=["get-data"])
-async def get_data(compound_id: str, type: str) -> Response:
+async def get_data(compound_id: str,
+                   type: str,
+                   sql_type: str,
+                   get_mnum_rows: str,
+                   cro: Optional[str] = None,
+                   assay: Optional[str] = None,
+                   atp_conc: Optional[float] = None,
+                   target: Optional[str] = None,
+                   variant: Optional[str] = None,
+                   cofactors: Optional[str] = None,
+                   modifier: Optional[str] = None
+                   ) -> Response:
     if not compound_id.startswith('FT') and len(compound_id) != 8:
         raise HTTPException(status_code=404, detail=f"{compound_id} is invalid")
-    results = get_table_data(compound_id, type)
+    payload = {}
+    payload['COMPOUND_ID'] = compound_id
+    payload['GET_M_NUM_ROWS'] = eval(get_mnum_rows.title())
+    payload['TYPE'] = type.upper()
+    payload['SQL_TYPE'] = sql_type.upper()
+    if cro:
+        payload['CRO'] = cro
+    if target:
+        payload['TARGET'] = target.upper()
+    if variant:
+        payload['VARIANT'] = variant.upper()
+    if cofactors:
+        payload['COFACTORS'] = cofactors.upper()
+    if assay:
+        payload['ASSAY_TYPE'] = assay
+    if atp_conc:
+        payload['ATP_CONC_UM'] = atp_conc
+    if modifier:
+        payload['MODIFIER'] = modifier.upper()
+    sql_stmt, return_payload = generate_sql_stmt(payload)
+    results = get_table_data(sql_stmt, return_payload)
     return results
 
+
 @app.post("/v1/change-data", tags=["post-data"])
-async def update_data(request: Request):
+async def update_data(sql_type: str, type: str, request: Request):
     payload = await request.json()
     if not payload["BATCH_ID"].startswith('FT') and \
         len(payload["BATCH_ID"]) != 8:
@@ -40,6 +75,10 @@ async def update_data(request: Request):
     if payload["FLAG"] not in [0, 1]:
         raise HTTPException(status_code=405,
                             detail=f"{payload['FLAG']} is not acceptable")
-    post_result = update_table_data(payload)
+    payload['SQL_TYPE'] = sql_type.upper()
+    payload['TYPE'] = type.upper()
+    sql_stmt, rtn_payload = generate_sql_stmt(payload)
+    print(payload)
+    post_result = generic_oracle_query(sql_stmt, payload)
     post_result = post_result | dict(STATUS_CODE=status.HTTP_200_OK)
     return post_result
--- backend/app/db.py
+++ backend/app/db.py
@@ -3,74 +3,99 @@ from .credentials import cred_dct
 from os import getenv
 
 
-def get_table_data(cmp_id, type):
+def generate_sql_stmt(payload):
+    sql_stmt = None
+    if payload["SQL_TYPE"] == "GET":
+        sql_stmt = f"""SELECT t0.CRO,
+                t0.ASSAY_TYPE,
+                t0.COMPOUND_ID,
+                t0.EXPERIMENT_ID,
+                t0.BATCH_ID,
+                t0.TARGET,
+                t0.VARIANT,
+                t0.COFACTORS,
+                t0.ATP_CONC_UM,
+                t0.MODIFIER,
+                t0.GRAPH,
+                t0.prop1,
+                ROUND(t0.ic50_nm,2),
+                t0.GEOMEAN_NM,
+                t0.flag,
+                t0.PID
+              FROM (
+            SELECT
+                t3.CRO,
+                t3.ASSAY_TYPE,
+                t3.COMPOUND_ID,
+                t3.EXPERIMENT_ID,
+                t3.BATCH_ID,
+                t3.TARGET,
+                t3.VARIANT,
+                t3.COFACTORS,
+                t3.ATP_CONC_UM,
+                t3.MODIFIER,
+                BASE64ENCODE(t3.GRAPH) as GRAPH,
+                t3.prop1,
+                t3.ic50_nm,
+                t3.flag,
+                t3.PID,
+             ROUND( POWER(10,
+               AVG( LOG(10, t3.ic50) ) OVER(PARTITION BY
+                    t3.CRO,
+                    t3.ASSAY_TYPE,
+                    t3.COMPOUND_ID,
+                    t3.TARGET,
+                    t3.VARIANT,
+                    t3.COFACTORS,
+                    t3.ATP_CONC_UM,
+                    t3.MODIFIER,
+                    t3.flag
+                )) * TO_NUMBER('1.0e+09'), 1) AS geomean_nM
+                FROM (
+              SELECT t1.CRO,
+                     t1.ASSAY_TYPE,
+                     t1.experiment_id,
+                     t1.COMPOUND_ID,
+                     t1.BATCH_ID,
+                     t1.TARGET,
+                     t1.VARIANT,
+                     t1.COFACTORS,
+                     t1.ATP_CONC_UM,
+                     t1.MODIFIER,
+                     t1.GRAPH,
+                     t2.flag,
+                     t2.prop1,
+                     t1.ic50,
+                     t1.ic50_nm,
+                     t1.PID
+               FROM DS3_USERDATA.COPY_ENZYME_INHIBITION_VW t1
+              INNER JOIN DS3_USERDATA.TEST_BIOCHEM_IC50_FLAGS t2
+                 ON t1.experiment_id = t2.experiment_id
+                AND t1.batch_id = t2.batch_id
+                AND nvl(t1.target,'-') = nvl(t2.target, '-')
+                AND nvl(t1.variant, '-') = nvl(t2.variant, '-')
+              WHERE
+                    t1.ASSAY_INTENT = 'Screening'
+                AND t1.VALIDATED = 'VALIDATED'
+              ) t3
+              ) t0
+              WHERE t0.COMPOUND_ID = '{payload['COMPOUND_ID']}'
+              """
+        if payload['GET_M_NUM_ROWS']:
+            sql_stmt += f""" AND t0.CRO = '{payload["CRO"]}'
+              AND t0.MODIFIER {'IS NULL' if payload["MODIFIER"] == 'NULL' or payload["MODIFIER"] is None else f"= '{payload['MODIFIER']}'"}
+              AND t0.ATP_CONC_UM = {payload["ATP_CONC_UM"]}
+              AND t0.ASSAY_TYPE = '{payload["ASSAY_TYPE"]}'
+              AND t0.TARGET = '{payload["TARGET"]}'
+              AND t0.VARIANT {'IS NULL' if payload["VARIANT"] == 'NULL' or payload["VARIANT"] is None else f"= '{payload['VARIANT']}'"}
+              AND t0.COFACTORS {'IS NULL' if payload["COFACTORS"] == 'NULL' or payload["COFACTORS"] is None else f"= '{payload['COFACTORS']}'"}
+           """
 
-    output = None
-    with OracleConnection(cred_dct['USERNAME'],
-                          cred_dct['PASSWORD'],
-                          cred_dct['HOST'],
-                          cred_dct['PORT'],
-                          cred_dct['SID']) as con:
-
-        sql_stmt = """SELECT T0.EXPERIMENT_ID, T0.BATCH_ID,
-                        CAST(SUBSTR(T0.BATCH_ID, 10,2) AS INT) AS BATCH_NUMBER,
-                        T0.TARGET, T0.VARIANT, T0.FLAG, T0.PROP1,
-                        ROUND(T1.IC50_NM, 2),
-                        T1.COFACTORS,
-                        BASE64ENCODE(T1.GRAPH)
-                        FROM DS3_USERDATA."""
-        if type == 'cellular':
-            sql_stmt += "TEST_CELLULAR_IC50_FLAGS T0"
-        elif type == 'biochem':
-            sql_stmt += "TEST_BIOCHEM_IC50_FLAGS T0"
-
-        sql_stmt += f""" INNER JOIN DS3_USERDATA.ENZYME_INHIBITION_VW T1
-                        ON T0.EXPERIMENT_ID = T1.EXPERIMENT_ID
-                        AND T0.BATCH_ID = T1.BATCH_ID
-                        AND NVL(T0.TARGET, '-') = NVL(T1.TARGET, '-')
-                        AND NVL(T0.VARIANT,'-') = NVL(T1.VARIANT,'-')
-                        WHERE T0.BATCH_ID LIKE '{cmp_id}%' ORDER BY BATCH_NUMBER,
-                        EXPERIMENT_ID, TARGET
-                    """
-        if getenv("INSTANCE_TYPE", None) is None:
-            print(sql_stmt)
-
-        with con.cursor() as cursor:
-            cursor.execute(sql_stmt)
-            output = cursor.fetchall()
-            if not output:
-                print(f"No data fetched for {cmp_id}")
-                return []
-            output_lst = []
-            for i, r in enumerate(output):
-                output_dct = {}
-                output_dct['ID'] = i
-                output_dct['EXPERIMENT_ID'] = r[0]
-                output_dct['BATCH_ID'] = r[1]
-                output_dct['TARGET'] = r[3]
-                output_dct['VARIANT'] = r[4]
-                output_dct['FLAG'] = r[5]
-                output_dct['PROP1'] = r[6]
-                output_dct['IC50_NM'] = r[7]
-                output_dct['COFACTORS'] = r[8]
-                output_dct['PLOT'] = r[9].read().rstrip().replace("\r\n", "")
-                output_lst.append(output_dct)
-        # print(output_lst)
-        return output_lst
-
-
-def update_table_data(payload):
-
-    with OracleConnection(cred_dct['USERNAME'],
-                          cred_dct['PASSWORD'],
-                          cred_dct['HOST'],
-                          cred_dct['PORT'],
-                          cred_dct['SID']) as con:
-
+    elif payload["SQL_TYPE"] == 'UPDATE':
         sql_stmt = "UPDATE DS3_USERDATA."
-        if payload['TYPE'] == 'cellular':
+        if payload['TYPE'] == 'CELLULAR':
             sql_stmt += "TEST_CELLULAR_IC50_FLAGS"
-        elif payload['TYPE'] == 'biochem':
+        elif payload['TYPE'] == 'BIOCHEM':
             sql_stmt += "TEST_BIOCHEM_IC50_FLAGS"
 
         sql_stmt += f""" SET FLAG = {payload["FLAG"]}
@@ -80,6 +105,61 @@ def update_table_data(payload):
                          AND VARIANT {'IS NULL' if payload["VARIANT"] == 'None' or payload["VARIANT"] is None else f"= '{payload['VARIANT']}'"}
                          AND PROP1 = {payload["PROP1"]}
                      """
+
+    return sql_stmt, payload
+
+
+def get_table_data(sql_stmt, payload):
+
+    output = None
+    with OracleConnection(cred_dct['USERNAME'],
+                          cred_dct['PASSWORD'],
+                          cred_dct['HOST'],
+                          cred_dct['PORT'],
+                          cred_dct['SID']) as con:
+
+        if getenv("INSTANCE_TYPE", None) is None:
+            print(sql_stmt)
+
+        with con.cursor() as cursor:
+            cursor.execute(sql_stmt)
+            output = cursor.fetchall()
+            if not output:
+                print(f"No data fetched for {payload['COMPOUND_ID']}")
+                return []
+            output_lst = []
+            for i, r in enumerate(output):
+                output_dct = {}
+                output_dct['ID'] = i
+                output_dct['CRO'] = r[0]
+                output_dct['ASSAY_TYPE'] = r[1]
+                output_dct['COMPOUND_ID'] = r[2]
+                output_dct['EXPERIMENT_ID'] = r[3]
+                output_dct['BATCH_ID'] = r[4]
+                output_dct['TARGET'] = r[5]
+                output_dct['VARIANT'] = r[6]
+                output_dct['COFACTORS'] = r[7]
+                output_dct['ATP_CONC_UM'] = r[8]
+                output_dct['MODIFIER'] = r[9]
+                output_dct['PLOT'] = r[10].read().rstrip().replace("\r\n", "")
+                output_dct['PROP1'] = r[11]
+                output_dct['IC50_NM'] = r[12]
+                output_dct['GEOMEAN'] = r[13]
+                output_dct['FLAG'] = r[14]
+                output_lst.append(output_dct)
+
+        print(output_dct['GEOMEAN'])
+        return output_lst
+
+
+def generic_oracle_query(sql_stmt, payload):
+
+    with OracleConnection(cred_dct['USERNAME'],
+                          cred_dct['PASSWORD'],
+                          cred_dct['HOST'],
+                          cred_dct['PORT'],
+                          cred_dct['SID']) as con:
+
         if getenv("INSTANCE_TYPE", None) is None:
             print(sql_stmt)
 
