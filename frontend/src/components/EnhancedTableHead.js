2a2307760ac428dd54c8d2b07ebe68abeb7255c0
spencer.trinh <spencer.trinh@kinnate.com>
2022-10-26 18:22:44 -0700

    dynamic column import for agg stats vs individual ic50 vals

--- frontend/src/components/EnhancedTableHead.js
+++ frontend/src/components/EnhancedTableHead.js
@@ -1,29 +1,52 @@
-// import { columns } from "./TableColumns";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import TableSortLabel from "@mui/material/TableSortLabel";
+import { IconButton } from "@mui/material";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import { PurpleColour } from "./Colour";
 
-// var columns = [];
+const styles = {
+  minWidth: 30,
+  fontWeight: "bold",
+  backgroundColor: PurpleColour,
+  color: "#efeff6ff",
+};
 
 export default function EnhancedTableHead(props) {
-  const { order, orderBy, onRequestSort, type } = props;
+  const { order, orderBy, onRequestSort, type, handleFilterIconClick } = props;
 
   const createSortHandler = (property) => (event) => {
     onRequestSort(event, property);
   };
   var DataFields = null;
 
-  if (type === "cellular") {
-    DataFields = require("./TableColumnsCellular");
-  } else {
-    DataFields = require("./TableColumnsBiochem");
+  switch (type) {
+    case "cellular_all":
+      DataFields = require("./TableColumnsCellularAll");
+      break;
+    case "cellular_agg":
+      DataFields = require("./TableColumnsCellularAll");
+      break;
+    case "biochem_all":
+      DataFields = require("./TableColumnsBiochemAll");
+      break;
+    case "biochem_agg":
+      DataFields = require("./TableColumnsBiochemAll");
+      break;
+    case "cellular_stats":
+      DataFields = require("./TableColumnsCellularStats");
+      break;
+    default:
+      DataFields = require("./TableColumnsBiochemStats");
   }
 
   return (
     <TableHead>
       <TableRow>
         {DataFields.columns.map((column) => (
+				  column.label === 'GEOMEAN' && type.includes('stats') ? 
+																								null :
           <TableCell
             key={column.id}
             align={column.align}
@@ -46,18 +69,21 @@ export default function EnhancedTableHead(props) {
               </TableSortLabel>
             )}
           </TableCell>
+				
         ))}
-        <TableCell
-          key={"ACTION"}
-          align={"center"}
-          style={{
-            minWidth: 30,
-            fontWeight: "bold",
-            backgroundColor: "#343990ff",
-            color: "#efeff6ff",
-          }}
-        >
-          Action
+        {type.includes("all") && (
+          <TableCell key={"ACTION"} align={"center"} style={styles}>
+            Action
+          </TableCell>
+        )}
+        <TableCell key={"FILTER"} alight={"center"} style={styles}>
+          <IconButton
+            color="inherit"
+            aria-label="filterButton"
+            onClick={handleFilterIconClick}
+          >
+            <FilterListIcon />
+          </IconButton>
         </TableCell>
       </TableRow>
     </TableHead>
