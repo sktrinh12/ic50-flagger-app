bc9293dfbb52f5b8f46f412c8a213f5f021c5869
spencer.trinh <spencer.trinh@kinnate.com>
2022-11-17 19:42:02 -0800

    add tooltip and textfield with styles

--- frontend/src/components/RadioButton.js
+++ frontend/src/components/RadioButton.js
@@ -4,20 +4,58 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
+import TextField from "@mui/material/TextField";
+import Tooltip from "@mui/material/Tooltip";
 
-export default function RadioButtonsGroup({ flagValue, handleEditFormChange }) {
+export default function RadioButtonsGroup({
+  flag,
+  data,
+  commentRef,
+  handleEditFormChange,
+}) {
   return (
     <FormControl>
       <FormLabel></FormLabel>
       <RadioGroup
         aria-labelledby="demo-controlled-radio-buttons-group"
         name="controlled-radio-buttons-group"
-        value={flagValue}
+        value={flag}
         onChange={handleEditFormChange}
       >
         <FormControlLabel value="include" control={<Radio />} label="Include" />
-        <FormControlLabel value="exclude" control={<Radio />} label="Exclude" />
+        <FormControlLabel
+          value="exclude"
+          control={<Radio />}
+          label="Exclude"
+          style={{ marginTop: "-12px" }}
+        />
       </RadioGroup>
+
+      <Tooltip
+        title={data.CHANGE_DATE}
+        arrow
+        open={true}
+        placement="bottom"
+        PopperProps={{
+          modifiers: [
+            {
+              name: "offset",
+              options: {
+                offset: [0, -10],
+              },
+            },
+          ],
+        }}
+      >
+        <TextField
+          label="Comments"
+          multiline
+          rows={4}
+          ref={commentRef}
+          defaultValue={data.COMMENT_TEXT}
+          helperText={data.USER_NAME}
+        />
+      </Tooltip>
     </FormControl>
   );
 }
