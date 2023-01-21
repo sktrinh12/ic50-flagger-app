28838241bfc22c373e108d45c188ad70b22e43bb
spencer.trinh <spencer.trinh@kinnate.com>
2023-01-21 00:28:02 -0800

    change cell incubation hr and assay type names in param query string

--- frontend/src/components/ReadRow.js
+++ frontend/src/components/ReadRow.js
@@ -1,8 +1,8 @@
-import * as React from "react";
-import TableCell from "@mui/material/TableCell";
-import TableRow from "@mui/material/TableRow";
-import ReactLoading from "react-loading";
-import { PurpleColour } from "./Colour";
+import * as React from 'react'
+import TableCell from '@mui/material/TableCell'
+import TableRow from '@mui/material/TableRow'
+import ReactLoading from 'react-loading'
+import { PurpleColour } from './Colour'
 
 const ReadRow = ({
   keyValue,
@@ -13,132 +13,132 @@ const ReadRow = ({
   handleEditClick,
 }) => {
   const handleDynamicValue = (columnName, value) => {
-    if (columnName === "FLAG") {
+    if (columnName === 'FLAG') {
       switch (value) {
         case 0:
-          value = "include";
-          break;
+          value = 'include'
+          break
         case 1:
-          value = "exclude";
-          break;
+          value = 'exclude'
+          break
         default:
-          break;
+          break
       }
     }
 
-    if (["VARIANT", "COFACTORS", "PASSAGE_NUMBER"].includes(columnName)) {
+    if (['VARIANT', 'COFACTORS', 'PASSAGE_NUMBER'].includes(columnName)) {
       if (value == null) {
-        value = "-";
+        value = '-'
       }
     }
 
-    if (columnName === "PLOT") {
-      return <img src={"data:image/png;base64, " + value} alt="EMPTY GRAPH" />;
+    if (columnName === 'PLOT') {
+      return <img src={'data:image/png;base64, ' + value} alt='EMPTY GRAPH' />
     }
-    return value;
-  };
+    return value
+  }
 
-  var DataFields = null;
-  let checkIfCellular = "CELL_INCUBATION_HR" in data;
+  var DataFields = null
+  let checkIfCellular = 'CELL_INCUBATION_HR' in data
   if (checkIfCellular) {
-    if ("N_OF_M" in data) {
-      DataFields = require("./TableColumnsCellularStats");
+    if ('N_OF_M' in data) {
+      DataFields = require('./TableColumnsCellularStats')
     } else {
-      DataFields = require("./TableColumnsCellularAll");
+      DataFields = require('./TableColumnsCellularAll')
     }
   } else {
-    if ("N_OF_M" in data) {
-      DataFields = require("./TableColumnsBiochemStats");
+    if ('N_OF_M' in data) {
+      DataFields = require('./TableColumnsBiochemStats')
     } else {
-      DataFields = require("./TableColumnsBiochemAll");
+      DataFields = require('./TableColumnsBiochemAll')
     }
   }
 
   return (
     <TableRow
       hover
-      role="checkbox"
+      role='checkbox'
       tabIndex={-1}
       key={keyValue}
       selected={columnLoading}
     >
       {DataFields.columns.map((column, i) => {
-        const value = data[column.id];
+        const value = data[column.id]
         let urlArray = [
-          "/get-data?compound_id=",
+          '/get-data?compound_id=',
           data.COMPOUND_ID,
-          "&type=",
-          types[0].split("_")[0] + "_agg",
-          "&sql_type=",
+          '&type=',
+          types[0].split('_')[0] + '_agg',
+          '&sql_type=',
           types[1],
-          "&cro=",
+          '&cro=',
           data.CRO,
-          "&assay=",
+          '&assay_type=',
           data.ASSAY_TYPE,
-          "&get_mnum_rows=false",
-          "&variant=",
-          !data.VARIANT || data.VARIANT === "-" ? "null" : data.VARIANT,
-          "&user_name=",
+          '&get_mnum_rows=false',
+          '&variant=',
+          !data.VARIANT || data.VARIANT === '-' ? 'null' : data.VARIANT,
+          '&user_name=',
           username,
-        ];
+        ]
 
-				checkIfCellular
+        checkIfCellular
           ? urlArray.push(
-                "&cell_line=",
-                data.CELL_LINE,
-                "&pct_serum=",
-                data.PCT_SERUM,
-                "&atp_conc_um=",
-                data.ATP_CONC_UM,
-                "&cell_incu_hr=",
-                data.CELL_INCUBATION_HR,
+              '&cell_line=',
+              data.CELL_LINE,
+              '&pct_serum=',
+              data.PCT_SERUM,
+              '&atp_conc_um=',
+              data.ATP_CONC_UM,
+              '&cell_incubation_hr=',
+              data.CELL_INCUBATION_HR
             )
           : urlArray.push(
-                "&target=",
-                data.TARGET,
-                "&atp_conc_um=",
-                data.ATP_CONC_UM,
-                "&cofactors=",
-                !data.COFACTORS || data.COFACTORS === "-"
-                  ? "null"
-                  : data.COFACTORS,
-            );
+              '&target=',
+              data.TARGET,
+              '&atp_conc_um=',
+              data.ATP_CONC_UM,
+              '&cofactors=',
+              !data.COFACTORS || data.COFACTORS === '-'
+                ? 'null'
+                : data.COFACTORS
+            )
 
-        let getURL = urlArray.join("");
+        let getURL = urlArray.join('')
 
-        // console.log(`the nested URL: ${getURL}`);
+        // console.log(`the nested URL: ${getURL}`)
 
         return (
           <TableCell align={column.align} key={`${keyValue}-${i}`}>
-            {columnLoading && column.id === "GEOMEAN" ? (
-              <div style={{ margin: "auto", padding: "0px" }}>
+            {columnLoading && column.id === 'GEOMEAN' ? (
+              <div style={{ margin: 'auto', padding: '0px' }}>
                 <ReactLoading
-                  type="spin"
+                  type='spin'
                   color={PurpleColour}
                   height={30}
                   width={30}
                 />
               </div>
-            ) : column.id === "GEOMEAN" && data.N_OF_M ? (
-              <a href={getURL} target="_blank" rel="noopener noreferrer">
+            ) : column.id === 'GEOMEAN' && data.N_OF_M ? (
+              <a href={getURL} target='_blank' rel='noopener noreferrer'>
                 {value}
               </a>
             ) : (
               handleDynamicValue(column.id, value)
             )}
           </TableCell>
-        );
+        )
       })}
       {data && !data.N_OF_M && (
         <TableCell
-          align={"center"}
+          align={'center'}
           key={`ACTION-${keyValue}-${
-            parseInt(keyValue.split("-")[2].replace(/^\D+/g, "")) + 5000
+            parseInt(keyValue.split('-')[2].replace(/^\D+/g, '')) + 5000
           }`}
         >
           <>
             <button
-              type="button"
+              type='button'
               onClick={(event) => handleEditClick(event, data)}
             >
               Edit
@@ -147,7 +147,7 @@ const ReadRow = ({
         </TableCell>
       )}
     </TableRow>
-  );
-};
+  )
+}
 
-export default ReadRow;
+export default ReadRow
