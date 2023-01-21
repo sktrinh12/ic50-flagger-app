bdb678d424df0bc3378fe8d32f95c301e5ec5717
spencer.trinh <spencer.trinh@kinnate.com>
2023-01-21 00:30:33 -0800

    change asasy type and cell incubation hr names, set cofactors/atp to null

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -8,7 +8,7 @@ class GetDataSchema(BaseModel):
     type: str
     sql_type: str
     cro: Optional[str] = None
-    assay: Optional[str] = None
+    assay_type: Optional[str] = None
     n_limit: Optional[int] = 20
     compound_id: Optional[str]
     get_mnum_rows: Optional[str] = "false"
@@ -17,9 +17,9 @@ class GetDataSchema(BaseModel):
     passage_nbr: Optional[str] = None
     # BIOCHEM
     atp_conc_um: Optional[str] = None
-    target: Optional[str] = None
-    cofactors: Optional[str] = None
+    target: Optional[str] = "null"
+    cofactors: Optional[str] = "null"
     # CELLULAR
     cell_line: Optional[str] = None
     pct_serum: Optional[int] = None
-    cell_incu_hr: Optional[int] = None
+    cell_incubation_hr: Optional[int] = None
