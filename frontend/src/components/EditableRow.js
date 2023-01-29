5ce6ff3b7072d804e73ea68a91916d1330ca9090
spencer.trinh <spencer.trinh@kinnate.com>
2023-01-29 15:05:18 -0800

    remove role=checkbox for testing & auto format

--- frontend/src/components/EditableRow.js
+++ frontend/src/components/EditableRow.js
@@ -1,7 +1,7 @@
-import RadioButtonsGroup from "./RadioButton";
-import * as React from "react";
-import TableCell from "@mui/material/TableCell";
-import TableRow from "@mui/material/TableRow";
+import RadioButtonsGroup from './RadioButton'
+import * as React from 'react'
+import TableCell from '@mui/material/TableCell'
+import TableRow from '@mui/material/TableRow'
 
 const EditableRow = ({
   keyValue,
@@ -14,7 +14,7 @@ const EditableRow = ({
   const renderContent = (param, column) => {
     return (
       <>
-        {[0, 1, "include", "exclude"].includes(param) && column === "FLAG" ? (
+        {[0, 1, 'include', 'exclude'].includes(param) && column === 'FLAG' ? (
           <RadioButtonsGroup
             flag={flag}
             data={data}
@@ -23,47 +23,47 @@ const EditableRow = ({
           />
         ) : param ? (
           param.length > 60 ? (
-            <img src={"data:image/png;base64, " + param} alt="EMPTY" />
+            <img src={'data:image/png;base64, ' + param} alt='EMPTY' />
           ) : (
             param
           )
         ) : (
-          "-"
+          '-'
         )}
       </>
-    );
-  };
+    )
+  }
 
-  var DataFields = null;
-  if ("WASHOUT" in data) {
-    DataFields = require("./TableColumnsCellularAll");
+  var DataFields = null
+  if ('WASHOUT' in data) {
+    DataFields = require('./TableColumnsCellularAll')
   } else {
-    DataFields = require("./TableColumnsBiochemAll");
+    DataFields = require('./TableColumnsBiochemAll')
   }
 
   return (
-    <TableRow hover role="checkbox" tabIndex={-1} key={keyValue}>
+    <TableRow hover tabIndex={-1} key={keyValue}>
       {DataFields.columns.map((column, i) => {
-        const value = data[column.id];
+        const value = data[column.id]
         return (
           <TableCell align={column.align} key={`${keyValue}-${i}`}>
             {renderContent(value, column.id)}
           </TableCell>
-        );
+        )
       })}
       <TableCell
-        align={"center"}
-        key={`ACTION-${parseInt(keyValue.replace(/^\D+/g, "")) + 5000}`}
+        align={'center'}
+        key={`ACTION-${parseInt(keyValue.replace(/^\D+/g, '')) + 5000}`}
       >
         <>
-          <button type="submit">Save</button>
-          <button type="button" onClick={handleCancelClick}>
+          <button type='submit'>Save</button>
+          <button type='button' onClick={handleCancelClick}>
             Cancel
           </button>
         </>
       </TableCell>
     </TableRow>
-  );
-};
+  )
+}
 
-export default EditableRow;
+export default EditableRow
