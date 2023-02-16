519ec8f6ac6a36691839bc72e1c2a970f5fb2b87
spencer.trinh <spencer.trinh@kinnate.com>
2023-02-16 11:47:38 -0800

    test new ui on Home

--- frontend/src/__test__/Home.test.jsx
+++ frontend/src/__test__/Home.test.jsx
@@ -1,9 +1,40 @@
 import './setupTest.js'
 import { render, screen } from './setupTest.js'
+import { within, fireEvent } from '@testing-library/react'
 import Home from '../components/Home'
 
-test('Check Home endpoint', () => {
-  render(<Home />)
-  const textElement = screen.getByText('HOME ENDPOINT')
-  expect(textElement).toBeInTheDocument()
+describe('Home check text', () => {
+  it('should render heading', () => {
+    render(<Home />)
+    // heading
+    const textElement = screen.getByText('Kinnate Geomean Viewer')
+    expect(textElement).toBeInTheDocument()
+    // label
+    const textField = screen.getByText('Compound ID')
+    expect(textField).toBeInTheDocument()
+    // button
+    const button = screen.getByText('Search')
+    expect(button).toBeInTheDocument()
+    // radio group
+    const radioGroup = screen.getByRole('radiogroup')
+    const { getByLabelText } = within(radioGroup)
+    const option1 = getByLabelText('Biochemical')
+    const option2 = getByLabelText('Cellular')
+
+    expect(option1).toBeInTheDocument()
+    expect(option2).toBeInTheDocument()
+  })
+
+  it('should update the TextField when typed into', async () => {
+    render(<Home />)
+
+    const compoundID = 'FT003421'
+    const autocomplete = await screen.findByLabelText('Compound ID')
+    fireEvent.click(autocomplete)
+
+    let input = screen.getByLabelText('Compound ID', { selector: 'input' })
+    fireEvent.change(input, { target: { value: compoundID } })
+
+    expect(input).toHaveValue(compoundID)
+  })
 })
