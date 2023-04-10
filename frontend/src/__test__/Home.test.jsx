4a767629e97b216e5a1116768384ddc5a5170577
spencer.trinh <spencer.trinh@kinnate.com>
2023-04-10 11:00:55 -0700

    change label text for autocomplete component

--- frontend/src/__test__/Home.test.jsx
+++ frontend/src/__test__/Home.test.jsx
@@ -10,7 +10,7 @@ describe('Home check text', () => {
     const textElement = screen.getByText('Kinnate Geomean Viewer')
     expect(textElement).toBeInTheDocument()
     // label
-    const textField = screen.getByText('Compound ID')
+    const textField = screen.getByText('compound-id-for-testing')
     expect(textField).toBeInTheDocument()
     // button
     const button = screen.getByText('Search')
@@ -29,10 +29,12 @@ describe('Home check text', () => {
     render(<Home />)
 
     const compoundID = 'FT003421'
-    const autocomplete = await screen.findByLabelText('Compound ID')
+    const autocomplete = await screen.findByLabelText('compound-id-for-testing')
     fireEvent.click(autocomplete)
 
-    let input = screen.getByLabelText('Compound ID', { selector: 'input' })
+    let input = screen.getByLabelText('compound-id-for-testing', {
+      selector: 'input',
+    })
     fireEvent.change(input, { target: { value: compoundID } })
 
     expect(input).toHaveValue(compoundID)
